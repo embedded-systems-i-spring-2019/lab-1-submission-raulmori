478c4fb750f19bd58106cc17c68dbc27f734e23c
raulmori <47406865+raulmori@users.noreply.github.com>
2019-04-28 21:06:19 -0400

    Update divider.top.vhd

--- Part1/divider.top.vhd
+++ Part1/divider.top.vhd
@@ -18,8 +18,9 @@ use IEEE.NUMERIC_STD.ALL;
 
 
 entity divider_top is
-  Port ( clk: in std_logic;
-         led0: out std_logic);
+      Port (
+            clk: in std_logic;
+             led0: out std_logic);
 end divider_top;
 
 
@@ -29,29 +30,26 @@ architecture Behavioral of divider_top is
     signal div_CE: std_logic := '0';        --This is just the "temporary" output of the clock
 
 
-        component clock_div                  --Here we  call all of clock.div by calling its entity parts.
-            port(clk: in std_logic;
+    component clock_div                  --Here we  call (INSTANTIATE) all of clock.div by calling its entity parts.
+            port(
+                clk: in std_logic;
                  div: out std_logic);
-        end component;
+    end component;
 
         
-        begin
-            U1: clock_div port map
-                    (clk => clk,    --We assign the entity "clk" (from clock.div)  into the temporary "clk" from divider.top
-                     div => div_CE);   --We assign the entity "div" (from clock.div)  into the temporary "div_ce" from divider.top 
-                   
+    begin
+         U1: clock_div port map          --We call create a PORT-MAP that works with the COMPONENT of the "CLOCK-ENABLE" which is the (ClOCK.DIV)
+                 (clk => clk,             --We assign the entity "clk" (from clock.div)  into the temporary "clk" from divider.top
+                  div => div_CE);             --We assign the entity "div" (from clock.div)  into the temporary "div_ce" from divider.top 
+            
 
-        process(clk)  --At every clock tick we will loop everything inside process
-                 begin  --This is the D Flip-Flop       
-                       if(clk'event and clk = '1') then   --If the temporary "clk" is "1" (This is just rising edge)  
-                           if(div_CE = '1') then   -- If the temporary "div_CE" is "1" (this is the output of the clock)
-                               led <= not led; --In this case since we know the temporary "led" is "0", we inverted to be "1" just like the clock output, then we reassign it to the temporary "led" again as "1"
-                               
-                           end if;
-                       end if;
-                    
-        
-                 led0 <= led;  --Here we just set the "led" value of "1" to the actual entity "led0", which is the LED of the zybo board.  
-        end process;
-
+         process(clk)  --At every clock tick we will loop everything inside process
+                  begin  --This is the D Flip-Flop. Here we use the Newly Added "CLOCK-ENABLE (CLOCK.DIV) to generate the LED-output Logic.      
+                        if(clk'event and clk = '1') then   --If the temporary "clk" is "1" (This is just RISING-EDGE)  
+                                if(div_CE = '1') then   -- If the temporary "div_CE" is "1" (this is the output of the clock)
+                                         led <= not led; --This is the "INPUT" to the D-Flip-Flop which is the output of the DflipFlop Inverted.      
+                                end if;
+                        end if;
+         led0 <= led;  --We then  reassign  the TEMPORARY "led" (Inverted) to the ENTITY-LED
+         end process;
 end Behavioral;
